d636cb95a5f4aef5dc495e9b785803b171a8c812
Marsel <tagrolka@yandex.ru>
2023-06-14 17:57:15 +0300

    Added reverse method to LinkedList

--- src/main/java/org/example/Main1.java
+++ src/main/java/org/example/Main1.java
@@ -1,67 +1,107 @@
 package org.example;
 
-class LinkedList {
+class LinkedList{
+
     private Node head;
     private Node tail;
 
-    public int get(int item) {
-        if (item > size()) throw new ArrayIndexOutOfBoundsException();// 1 08
-        Node tmp = head;
-        while (item > 0) {
-            tmp = tmp.next;
-            item--;
-        }
-
-        return tmp.value;
-
+    public int getHead() {
+        return head.value;
     }
 
-    public int size() {
+    public int getTail() {
+        return tail.value;
+    }
+
+    public void add(int value){
+        Node tempNode = new Node();
+        tempNode.value = value;
+        if (head != null){
+            if (size() == 1) tail = head;
+        tempNode.next = head;
+        head.previous = tempNode;
+        }
+        head = tempNode;
+    }
+
+    public void append(int value){
+        Node tempNode = new Node();
+        tempNode.value = value;
+        if (tail != null){
+            if (size() == 1) head = tail;
+        tempNode.previous = tail;
+        tail.next = tempNode;
+        }
+        tail = tempNode;
+    }
+
+    public int size(){
         int size = 1;
         if (head == null) return 0;
-        Node tmp = head;
-        while (tmp.next != null) {
-            tmp = tmp.next;
+        Node tempNode = head;
+        while (tempNode.next != null){
             size++;
+            tempNode = tempNode.next;
         }
         return size;
     }
 
-    public void remove() {
-        if (head != null)
-            head = head.next;
+    public void remove(){
+        if (head != null) head = head.next;
     }
 
-    public void add(int digit) {
-        Node node = new Node();
-        node.value = digit;
-        if (head != null){
-            if (size() == 1) {
-                tail = head;}
-            node.next = head;
-            head.previous = node;
+    public int getValueByIndex(int index){
+        if (index > size() - 1) throw new ArrayIndexOutOfBoundsException();
+        int i = 0;
+        Node tempNode = head;
+        while (i < index) {
+            tempNode = tempNode.next;
+            i++;
         }
-        head = node;
+        return tempNode.value;
+    }
+
+    public void reverse(){
+        Node current = head;
+        Node tempNode = null;
+        boolean tailFlag = true;
+
+        while (current != null){
+            tempNode = current.previous;
+            current.previous = current.next;
+            current.next = tempNode;
+            current = current.previous;
+            if (tailFlag){
+                tail = current.previous;
+                tailFlag = false;
+            }
+        }
+        if (tempNode != null){
+            head = tempNode.previous;
+        }
     }
 }
 
-class Node {
+
+
+class Node{
     public int value;
     public Node next;
     public Node previous;
 }
 
-public class Main1 {
-
+class Main1{
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.add(0);
         ll.add(1);
         ll.add(2);
+        ll.append(-1);
         ll.add(3);
-        ll.add(4);
+        ll.append(-2);
+        ll.reverse();
         for (int i = 0; i < ll.size(); i++) {
-            System.out.println(ll.get(i));
+            System.out.println(ll.getValueByIndex(i));
         }
     }
 }
